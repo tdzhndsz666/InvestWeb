95f8fe0da669dc4bac9e1695e1d21a0cdd14693b
lilai <lilai@yiyon.com.cn>
2018-11-21 20:07:14 +0800

    remove laboratory

--- laboratory/application/models/AdminInfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-    class AdminInfo extends Zend_Db_Table{
-            
-        protected $_name='admininfo';//表名
-        protected $_primary='id';//主键
-        
-    }
--- laboratory/application/models/OrderDetail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class OrderDetail extends Zend_Db_Table{
-    
-    protected $_name='orderdetail';//表名
-    protected $_primary='id';//指定主键,默认是id
-}
